190139535cc5599a7c487bb89f724c15f27227d0
David Nasrulloh <davidkrb52@gmail.com>
2023-08-11 23:56:01 +0700

    display all product in frontend web ecommerce

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function Index(){
-        return view('user_template.home');
+        $allproduct = Product::latest()->get();
+        return view('user_template.home', compact('allproduct'));
     }
 }
